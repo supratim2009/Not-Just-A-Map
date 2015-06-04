8d09be4dcca250641ead6e7969e5ed81844b05ef
01HW664133 <tcs5c2@01HHW664133.local>
2015-06-04 15:48:07 +0530

    Auto Layout Prepared

---
+++ Not_Just_A_Map/FactoryViewProvider.swift
@@ -0,0 +1,25 @@
+//
+//  FactoryViewProvider.swift
+//  Not_Just_A_Map
+//
+//  Created by 01HW664133 on 04/06/15.
+//  Copyright (c) 2015 Supratim&Co. All rights reserved.
+//
+
+import UIKit
+
+class FactoryViewProvider: NSObject {
+    class func getViewController (FromStroryBoard storyBoard :(UIStoryboard), ForMapType type :(String)) -> AnyObject? {
+        var viewController :AnyObject?
+//        switch type {
+//        case "AppleMap" :
+//            viewController = [UIStoryboard ]
+//        case "GoogleMap" :
+//            
+//        case "BingMap":
+//            
+//        default :
+//        }
+    return viewController
+    }
+}
---
+++ Not_Just_A_Map/ViewControllers/HomeMapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  HomeMapViewController.swift
+//  Not_Just_A_Map
+//
+//  Created by 01HW664133 on 04/06/15.
+//  Copyright (c) 2015 Supratim&Co. All rights reserved.
+//
+
+import UIKit
+
+
+class HomeMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func valueChanged(sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func navigationItemTapped(sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func leftToolBarButtonItemTapped(sender: UIBarButtonItem) {
+    }
+}
